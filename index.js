82af164555f5f697187bdf492171babf5af2ceac
Esteban <esteban.templier82@gmail.com>
2023-10-05 04:36:16 +0200

    ajout des réalisations, modification de la présentation, ajout du burger menu et du responsive

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// btnWorkSpace.addEventListener('click', dropDown( contentWorkSpace, isCheckedForBtnWorkSpace, btnWorkSpace ));
-// btnFrontend.addEventListener('click', dropDown( contentFrontend, isCheckedForBtnFrontend, btnFrontend ));
-// btnBackend.addEventListener('click', dropDown( contentBackend, isCheckedForBtnBackend, btnBackend ));
+//                    burger menu
 
-// function dropDown(content, isChecked, btn) {
-//     isChecked = !isChecked;
 
-//     if (isChecked === true) {
-//         btn.style.borderRadius = "7px 7px 0 0";
-//         content.style.padding = "5px";
-//         content.style.height = "auto";
-//     } else {
-//         btn.style.borderRadius = "7px";
-//         content.style.padding = "0";
-//         content.style.height = "0";
-//     }
-// };
+let isCheckedForIcon = false;
+
+document.getElementById('icon').addEventListener('click', function () {
+    isCheckedForIcon = !isCheckedForIcon;
+
+    if (isCheckedForIcon === true) {
+        document.querySelector('nav').setAttribute("class", "active");
+        document.querySelector('.navBar').style.boxShadow = "none";
+    } else {
+        document.querySelector('nav').removeAttribute("class", "active");
+        document.querySelector('.navBar').style.boxShadow = "0 6px 15px 0 rgba(0, 0, 0, 0.15)";
+    }
+})
+
+
+//                   Dropdown skills
+
 
 let contentWorkSpace = document.querySelector('.contentWorkSpace');
 let contentFrontend = document.querySelector('.contentFrontend');
@@ -70,3 +73,25 @@ btnBackend.addEventListener('click', function () {
         contentBackend.style.height = "0";
     }
 });
+
+
+//                   Hover over the container card
+
+
+const hoverCard1 = document.querySelector('.containerCard1');
+const hoverCard2 = document.querySelector('.containerCard2');
+
+const name1 = document.querySelector('.hoverCard1 p');
+const name2 = document.querySelector('.hoverCard2 p');
+
+hover(hoverCard1, name1);
+hover(hoverCard2, name2);
+
+function hover(hoverCard, name) {
+    hoverCard.addEventListener('mouseover', function () {
+        name.style.display = "block";
+    });
+    hoverCard.addEventListener('mouseout', function () {
+        name.style.display = "none";
+    });
+}
